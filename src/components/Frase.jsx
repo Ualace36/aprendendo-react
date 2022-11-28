e700a572660f97da6a3743f84dfaeee7f679a23d
Ualace36 <89046095+Ualace36@users.noreply.github.com>
2022-11-27 21:48:44 -0300

    criando os primeiros componentes

---
+++ src/components/Frase.jsx
@@ -0,0 +1,9 @@
+function Frase(){
+    return (
+        <div>
+            <h2>Este é um componente com uma frase</h2>
+        </div>
+    )
+}
+
+export default Frase
---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import './HelloWorld.scss'
+function HelloWorld(params) {
+
+ return (
+    <div className="divComponent">
+        <h1>Meu Primeiro components</h1>
+    </div>
+ ) 
+
+}
+
+export default HelloWorld
