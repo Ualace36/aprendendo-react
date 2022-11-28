9aa04e9d3a78e5338a7379114adc8497c5fcf430
Ualace36 <89046095+Ualace36@users.noreply.github.com>
2022-11-27 23:37:55 -0300

    implementanto props em nosso projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
 import './App.scss'
+import SayMyName from './components/SayMyName'
 import Frase from './components/Frase'
 import HelloWorld from './components/HelloWorld'
+import Pessoa from './components/Pessoa'
 
 function App() {
-  function sum(a,b) {
-    return a + b
-  }
-  const url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5Dt_eLH02JhaCdjrsJklnD2k_awbJIFK7CGKeBW6vGhe0DXU9Yhrp3yfnc1EsGeKG6VM&usqp=CAU"
-  const name = "Uálace"
-  //nome todo maiúsculo
-  const newName = name.toUpperCase()
-
   return (
     <div className="App">
       <h1>HelloWorld</h1>
-      <h1>Olá, {newName}</h1>
-      <h2><Frase></Frase></h2>
-      <p>Meus segundo projeto no ar</p>
-       <p>soma: {sum(2,5)}</p> 
-       <img src={url}alt="Animes: One Peace"   /> 
+      < SayMyName nome="Ualace"/>
+      < SayMyName nome="Arthur"/>
+      < Pessoa 
+      nome="Ualace" 
+      idade="37" 
+      profissao="programador" 
+      foto="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5Dt_eLH02JhaCdjrsJklnD2k_awbJIFK7CGKeBW6vGhe0DXU9Yhrp3yfnc1EsGeKG6VM&usqp=CAU"
+    />
        </div>
   )
 }
